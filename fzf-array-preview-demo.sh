a3125de4e9c78afaa5d9a57a53781697756200c4
slowpeek <slowpeek@users.noreply.github.com>
2021-06-29 00:40:00 +0300

    init

---
+++ fzf-array-preview-demo.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+# Copyright (c) 2021 https://github.com/slowpeek
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Homepage: https://github.com/slowpeek/fzf-array-preview
+
+countries=(
+
+"AFG=Afghanistan" "ALA=Aland Islands" "ALB=Albania" "DZA=Algeria"
+"ASM=American Samoa" "AND=Andorra" "AGO=Angola" "AIA=Anguilla"
+"ATA=Antarctica" "ATG=Antigua and Barbuda" "ARG=Argentina"
+"ARM=Armenia" "ABW=Aruba" "AUS=Australia" "AUT=Austria"
+"AZE=Azerbaijan" "BHS=Bahamas" "BHR=Bahrain" "BGD=Bangladesh"
+"BRB=Barbados" "BLR=Belarus" "BEL=Belgium" "BLZ=Belize" "BEN=Benin"
+"BMU=Bermuda" "BTN=Bhutan" "BOL=Bolivia" "BES=Bonaire, Sint Eustatius
+and Saba" "BIH=Bosnia and Herzegovina" "BWA=Botswana" "BVT=Bouvet
+Island" "BRA=Brazil" "IOT=British Indian Ocean Territory" "BRN=Brunei
+Darussalam" "BGR=Bulgaria" "BFA=Burkina Faso" "BDI=Burundi"
+"KHM=Cambodia" "CMR=Cameroon" "CAN=Canada" "CPV=Cape Verde"
+"CYM=Cayman Islands" "CAF=Central African Republic" "TCD=Chad"
+"CHL=Chile" "CHN=China" "CXR=Christmas Island" "CCK=Cocos (Keeling)
+Islands" "COL=Colombia" "COM=Comoros" "COG=Congo" "COD=Congo,
+Democratic Republic of the Congo" "COK=Cook Islands" "CRI=Costa Rica"
+"CIV=Cote D'Ivoire" "HRV=Croatia" "CUB=Cuba" "CUW=Curacao"
+"CYP=Cyprus" "CZE=Czech Republic" "DNK=Denmark" "DJI=Djibouti"
+"DMA=Dominica" "DOM=Dominican Republic" "ECU=Ecuador" "EGY=Egypt"
+"SLV=El Salvador" "GNQ=Equatorial Guinea" "ERI=Eritrea" "EST=Estonia"
+"ETH=Ethiopia" "FLK=Falkland Islands (Malvinas)" "FRO=Faroe Islands"
+"FJI=Fiji" "FIN=Finland" "FRA=France" "GUF=French Guiana" "PYF=French
+Polynesia" "ATF=French Southern Territories" "GAB=Gabon" "GMB=Gambia"
+"GEO=Georgia" "DEU=Germany" "GHA=Ghana" "GIB=Gibraltar" "GRC=Greece"
+"GRL=Greenland" "GRD=Grenada" "GLP=Guadeloupe" "GUM=Guam"
+"GTM=Guatemala" "GGY=Guernsey" "GIN=Guinea" "GNB=Guinea-Bissau"
+"GUY=Guyana" "HTI=Haiti" "HMD=Heard Island and Mcdonald Islands"
+"VAT=Holy See (Vatican City State)" "HND=Honduras" "HKG=Hong Kong"
+"HUN=Hungary" "ISL=Iceland" "IND=India" "IDN=Indonesia" "IRN=Iran,
+Islamic Republic of" "IRQ=Iraq" "IRL=Ireland" "IMN=Isle of Man"
+"ISR=Israel" "ITA=Italy" "JAM=Jamaica" "JPN=Japan" "JEY=Jersey"
+"JOR=Jordan" "KAZ=Kazakhstan" "KEN=Kenya" "KIR=Kiribati" "PRK=Korea,
+Democratic People's Republic of" "KOR=Korea, Republic of" "XKX=Kosovo"
+"KWT=Kuwait" "KGZ=Kyrgyzstan" "LAO=Lao People's Democratic Republic"
+"LVA=Latvia" "LBN=Lebanon" "LSO=Lesotho" "LBR=Liberia" "LBY=Libyan
+Arab Jamahiriya" "LIE=Liechtenstein" "LTU=Lithuania" "LUX=Luxembourg"
+"MAC=Macao" "MKD=Macedonia, the Former Yugoslav Republic of"
+"MDG=Madagascar" "MWI=Malawi" "MYS=Malaysia" "MDV=Maldives" "MLI=Mali"
+"MLT=Malta" "MHL=Marshall Islands" "MTQ=Martinique" "MRT=Mauritania"
+"MUS=Mauritius" "MYT=Mayotte" "MEX=Mexico" "FSM=Micronesia, Federated
+States of" "MDA=Moldova, Republic of" "MCO=Monaco" "MNG=Mongolia"
+"MNE=Montenegro" "MSR=Montserrat" "MAR=Morocco" "MOZ=Mozambique"
+"MMR=Myanmar" "NAM=Namibia" "NRU=Nauru" "NPL=Nepal" "NLD=Netherlands"
+"ANT=Netherlands Antilles" "NCL=New Caledonia" "NZL=New Zealand"
+"NIC=Nicaragua" "NER=Niger" "NGA=Nigeria" "NIU=Niue" "NFK=Norfolk
+Island" "MNP=Northern Mariana Islands" "NOR=Norway" "OMN=Oman"
+"PAK=Pakistan" "PLW=Palau" "PSE=Palestinian Territory, Occupied"
+"PAN=Panama" "PNG=Papua New Guinea" "PRY=Paraguay" "PER=Peru"
+"PHL=Philippines" "PCN=Pitcairn" "POL=Poland" "PRT=Portugal"
+"PRI=Puerto Rico" "QAT=Qatar" "REU=Reunion" "ROM=Romania" "RUS=Russian
+Federation" "RWA=Rwanda" "BLM=Saint Barthelemy" "SHN=Saint Helena"
+"KNA=Saint Kitts and Nevis" "LCA=Saint Lucia" "MAF=Saint Martin"
+"SPM=Saint Pierre and Miquelon" "VCT=Saint Vincent and the Grenadines"
+"WSM=Samoa" "SMR=San Marino" "STP=Sao Tome and Principe" "SAU=Saudi
+Arabia" "SEN=Senegal" "SRB=Serbia" "SCG=Serbia and Montenegro"
+"SYC=Seychelles" "SLE=Sierra Leone" "SGP=Singapore" "SXM=Sint Maarten"
+"SVK=Slovakia" "SVN=Slovenia" "SLB=Solomon Islands" "SOM=Somalia"
+"ZAF=South Africa" "SGS=South Georgia and the South Sandwich Islands"
+"SSD=South Sudan" "ESP=Spain" "LKA=Sri Lanka" "SDN=Sudan"
+"SUR=Suriname" "SJM=Svalbard and Jan Mayen" "SWZ=Swaziland"
+"SWE=Sweden" "CHE=Switzerland" "SYR=Syrian Arab Republic" "TWN=Taiwan,
+Province of China" "TJK=Tajikistan" "TZA=Tanzania, United Republic of"
+"THA=Thailand" "TLS=Timor-Leste" "TGO=Togo" "TKL=Tokelau" "TON=Tonga"
+"TTO=Trinidad and Tobago" "TUN=Tunisia" "TUR=Turkey"
+"TKM=Turkmenistan" "TCA=Turks and Caicos Islands" "TUV=Tuvalu"
+"UGA=Uganda" "UKR=Ukraine" "ARE=United Arab Emirates" "GBR=United
+Kingdom" "USA=United States" "UMI=United States Minor Outlying
+Islands" "URY=Uruguay" "UZB=Uzbekistan" "VUT=Vanuatu" "VEN=Venezuela"
+"VNM=Viet Nam" "VGB=Virgin Islands, British" "VIR=Virgin Islands,
+U.s." "WLF=Wallis and Futuna" "ESH=Western Sahara" "YEM=Yemen"
+"ZMB=Zambia" "ZWE=Zimbabwe"
+
+)
+
+# Remove line breaks.
+countries=("${countries[@]/$'\n'/ }")
+
+bye () {
+    printf '%s\n' "$1"
+    exit 1
+}
+
+hash fzf 2>/dev/null || bye "'fzf' tool not found"
+
+req=fzf-array-preview.sh
+# shellcheck disable=SC1090
+source "$req" 2>/dev/null || bye "'$req' not found"
+
+demos=(
+    'sq:number => its square'  # array
+    'rsq:number => its root'   # sparse array
+    'cc2c:country code => country' # assoc
+    'c2cc:country => country code' # assoc
+    'q:quit'
+)
+
+n=0
+for el in "${demos[@]%%:*}"; do
+    declare -n var=demo_$el
+    var=$((++n))
+done
+
+# shellcheck disable=SC2034
+typeset +n var
+unset -v var
+
+# shellcheck disable=SC2154,SC2034
+select name in "${demos[@]#*:}"; do
+    case $REPLY in
+        "$demo_sq")
+            sq=()
+            for ((i=0; i<=100; i++)); do
+                sq[i]=$((i*i))
+            done
+
+            fzf_array_preview sq
+            unset -v sq
+            ;;
+        "$demo_rsq")
+            rsq=()
+            for ((i=0; i<=100; i++)); do
+                rsq[i*i]=$i
+            done
+
+            fzf_array_preview rsq
+            unset -v rsq
+            ;;
+        "$demo_cc2c")
+            declare -A cc2c=()
+            for s in "${countries[@]}"; do
+                cc2c[${s%%=*}]=${s##*=}
+            done
+
+            fzf_array_preview cc2c
+            unset -v cc2c
+            ;;
+        "$demo_c2cc")
+            declare -A c2cc=()
+            for s in "${countries[@]}"; do
+                c2cc[${s##*=}]=${s%%=*}
+            done
+
+            fzf_array_preview c2cc
+            unset -v c2cc
+            ;;
+        "$demo_q")
+            break
+            ;;
+    esac
+done
---
+++ fzf-array-preview.sh
@@ -0,0 +1,66 @@
+# -*- mode: sh; sh-shell: bash; -*-
+
+# Copyright (c) 2021 https://github.com/slowpeek
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Homepage: https://github.com/slowpeek/fzf-array-preview
+
+# It works with arrays, sparse arrays and assoc arrays. Just call it
+# with a variable name as the only argument to make fzf render
+# key=>val mapping.
+#
+# Target shell: bash.
+#
+# The generic idea is taken from this reddit comment
+# https://www.reddit.com/r/bash/comments/o9djvn/arrays_with_fzf/h3alju1
+fzf_array_preview () {
+    if (($# < 1)); then
+        echo "${FUNCNAME[0]} requires a var name argument" >&2
+        return 1
+    fi
+
+    # All local vars should be prefixed with some junk to reduce risk
+    # of shadowing the var name passed as $1. Let it be 'fzfap_'.
+    local fzfap_type fzfap_def
+
+    if ! read -r fzfap_type fzfap_type fzfap_def < <(declare -p "$1" 2>/dev/null); then
+        echo "${FUNCNAME[0]}: '$1' is not defined" >&2
+        return 1
+    fi
+
+    if [[ ! $fzfap_type == *[aA]* ]]; then
+        echo "${FUNCNAME[0]}: '$1' is not an array" >&2
+        return 1
+    fi
+
+    # Use some safe var name in $fzfap_def instead of raw $1. For
+    # example $BASH_VERSINFO is always set and is readonly hence
+    # trying to declare it below in fzf call would fail.
+    fzfap_def="declare -A ${fzfap_def/$1/fzfap_it}"
+    local -n fzfap_var=$1
+
+    printf '%s\0' "${!fzfap_var[@]}" | sort -Vz |\
+        fzf -e --read0 --reverse --preview \
+            "$fzfap_def; printf '%s\n' \"\${fzfap_it[{}]}\""
+
+    return 0
+}
